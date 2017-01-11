7d0366da5d13ceed11659460b30a2a2d859330c2
Hrayr Movsisyan <lincerta@gmail.com>
2017-01-12 00:28:44 +0400

    st

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
+
+	/**
+	 * Home Services Tabs 
+	 */
 	$('.serviceOpener').on('click', function(e){
 		e.stopPropagation();
 		var target = $(this).attr('href');
 		$('.serviceOpener').removeClass('active');
 		$(this).addClass('active');
+
 		var tl = new TimelineMax();
-		tl.fromTo(target, .5, {display: 'none', opacity: 0}, {display: 'block', opacity: 1});
+		tl.staggerTo($('.serviceDescr').children(), .5, {autoAlpha: 0, display: 'none', opacity: 0}, .2)
+			.staggerFromTo($(target).children(), .5, {autoAlpha: 0, display: 'none', opacity: 0}, {autoAlpha: 1, display: 'block', opacity: 1}, .2);
 		$(target).show()
-	})
+	});
+
+
+
 }); /* end of document ready */
