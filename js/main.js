d13da8869a0fa68ebcd00f0e6e87a5f8123ac59e
hrayr <lincerta@gmail.com>
2017-01-12 17:52:23 +0400

    st

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	 * Home Services Tabs 
 	 */
 	$('.serviceOpener').on('click', function(e){
-		e.stopPropagation();
+		e.preventDefault();
 		var target = $(this).attr('href');
 		$('.serviceOpener').removeClass('active');
 		$(this).addClass('active');
@@ -14,6 +14,21 @@ $(document).ready(function(){
 			.staggerFromTo($(target).children(), .5, {autoAlpha: 0, display: 'none', opacity: 0}, {autoAlpha: 1, display: 'block', opacity: 1}, .2);
 		$(target).show()
 	});
+
+	/**
+	 * Progress bar circle
+	 */
+	$('.pie_progress').asPieProgress({
+        namespace: 'pie_progress',
+        speed: 40,
+        easing: 'ease',
+        size: 100,
+        trackcolor: '#151515',
+        fillcolor: 'rgba(255,255,255,.1)',
+        // barcolor: 'blue'
+
+    });
+
 
 
 
