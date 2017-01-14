8d3aea2f1a3d5a9296e2c3caf16aeaabe3d559ae
Hrayr Movsisyan <lincerta@gmail.com>
2017-01-15 01:34:35 +0400

    sass structure & home animations

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
 		$(target).show()
 	});
 
+
 	/**
 	 * Progress bar circle
 	 */
@@ -28,6 +29,64 @@ $(document).ready(function(){
         // barcolor: 'blue'
 
     });
+
+	/**
+	 * scrollr initialization
+	 */
+    var s = skrollr.init({
+    	// animate: true,
+    	// smoothScrolling: true,
+    	// smoothScrollingDuration: 500,
+    	// edgeStrategy: 'reset'
+    });
+
+
+    /**
+	 * scroll smoothly
+	 */
+	if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+	window.onmousewheel = document.onmousewheel = wheel;
+
+	var goUp = true;
+	var end = null;
+	var interval = null;
+
+	function wheel(event) {
+	    var delta = 0;
+	    if (event.wheelDelta) delta = event.wheelDelta / 120;
+	    else if (event.detail) delta = -event.detail / 3;
+
+	    handle(delta);
+	    if (event.preventDefault) event.preventDefault();
+	    event.returnValue = false;
+	}
+
+	function handle(delta) {
+	    var animationInterval = 10; //lower is faster
+	    var scrollSpeed = 30; //lower is faster
+
+	    if (end == null) {
+	        end = $(window).scrollTop();
+	    }
+	    end -= 20 * delta;
+	    goUp = delta > 0;
+
+	    if (interval == null) {
+	        interval = setInterval(function() {
+	            var scrollTop = $(window).scrollTop();
+	            var step = Math.round((end - scrollTop) / scrollSpeed);
+	            if (scrollTop <= 0 ||
+	                scrollTop >= $(window).prop("scrollHeight") - $(window).height() ||
+	                goUp && step > -1 ||
+	                !goUp && step < 1) {
+	                clearInterval(interval);
+	                interval = null;
+	                end = null;
+	            }
+	            $(window).scrollTop(scrollTop + step);
+	        }, animationInterval);
+	    }
+	}
 
 
 
